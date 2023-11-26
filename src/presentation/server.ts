1deb9a395441cfa648fb9c5a34a238ec7f68dd86
amarianjel <abrm.enginner@gmail.com>
2023-11-26 20:03:30 -0300

    CheckService - UseCase

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,29 +1,26 @@
-// import { CheckService } from '../domain/use-cases/checks/check-service';
-// import { CronService } from './cron/cron-service';
+import { CheckService } from '../domain/use-cases/checks/check-service';
+import { CronService } from './cron/cron-service';
+
+import { CronJob } from 'cron';
 
 
 export class Server {
 
     public static start() {
 
-    console.log( 'Server started...' );
+        console.log( 'Server started...' );
 
-    
-    // CronService.createJob(
-    //     '*/5 * * * * *',
-    //     () => {
-    //     const url = 'https://google.com';
-    //     new CheckService(
-    //         () => console.log( `${ url } is ok` ),
-    //         ( error ) => console.log( error ),
-    //     ).execute( url );
-    //     // new CheckService().execute( 'http://localhost:3000' );
-        
-    //     }
-    // );
-
-
+        CronService.createJob( 
+            '*/5 * * * * *',
+            () => {
+                const url = 'https://google.com';
+                new CheckService(
+                    () => console.log( `${ url } is ok` ),
+                    ( error ) => console.log( error ),
+                ).execute( url );
+                // new CheckService().execute( 'http://localhost:3000' );
+            
+            }
+        );
     }
-
-
 }
