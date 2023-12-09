c22b96b1ca7381f21b07fe6fc48fa21be449a47c
amarianjel <abrm.enginner@gmail.com>
2023-12-09 12:29:57 -0300

    Grabar Logs en Mongo 2

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import { envs } from '../config/plugins/envs.plugin';
+import { LogSeverityLevel } from '../domain/entities/log.entity';
 import { CheckService } from '../domain/use-cases/checks/check-service';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
@@ -8,15 +9,15 @@ import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email.service';
 
 const logRepository = new LogRepositoryImpl(
-    // new FileSystemDatasource(),
-    new MongoLogDatasource(),
+    new FileSystemDatasource(),
+    // new MongoLogDatasource(),
 );
 
 const emailService = new EmailService();
 
 export class Server {
 
-    public static start() {
+    public static async start() {
 
         console.log( 'Server started...' );
 
@@ -29,6 +30,12 @@ export class Server {
         // )
         // emailService.sendEmailWithFileSystemLogs(
         // );
+
+
+
+        //TODO: Ver logs
+        const logs = await logRepository.getLogs( LogSeverityLevel.high );
+        console.log(logs);
 
         // CronService.createJob( 
         //     '*/5 * * * * *',
