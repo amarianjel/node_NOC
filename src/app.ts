6d773674d34d4443a7e8a9f7ec77885e29e2a46a
amarianjel <abrm.enginner@gmail.com>
2023-12-09 12:08:30 -0300

    Crear y leer de Mongo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from "./config/plugins/envs.plugin";
-import { MongoDatabase } from "./data/mongo";
+import { LogModel, MongoDatabase } from "./data/mongo";
 import { Server } from "./presentation/server";
 
 (async() => {
@@ -13,6 +13,22 @@ async function main(){
         mongoUrl: envs.MONGO_URL,
         dbName: envs.MONGO_DB_NAME,
     })
+
+    // TODO: Crear una coleccion = tablas, documento = registro
+    // const newLog = await LogModel.create({
+    //     message: 'Test message desde mongo',
+    //     origin: 'App.ts',
+    //     level: 'low'
+    // });
+
+    // await newLog.save();
+
+    // console.log(newLog);
+
+
+    //TODO: Encontrar log
+    const logs = await LogModel.find();
+    console.log(logs);
 
     // Server.start();
 }
