a25a5cb2c37b3713141a6a39ed471edc8bbf2ef9
amarianjel <abrm.enginner@gmail.com>
2023-12-09 00:44:36 -0300

    Nodemailer - Gmail

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,9 @@
+import { envs } from '../config/plugins/envs.plugin';
 import { CheckService } from '../domain/use-cases/checks/check-service';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
 import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
 
 const fileSystemLogRepository = new LogRepositoryImpl(
     new FileSystemDatasource(),
@@ -15,6 +17,16 @@ export class Server {
         console.log( 'Server started...' );
 
         // TODO: Mandar Email
+        const emailService = new EmailService();
+        emailService.sendEmail({
+            to: 'abraham.marianjel@gmail.com',
+            subject: 'Teste de envio de email',
+            htmlBody: `
+                <h1>Teste de envio de email</h1>
+                <h3>LOG - NOC</h3>
+                <p>lorem velit</p>
+            `
+        })
 
         // CronService.createJob( 
         //     '*/5 * * * * *',
